18027d886252b42ac84e647666898df3b4eb2baf
edgarorb1 <95740372+edgarorb1@users.noreply.github.com>
2022-02-27 01:54:01 -0600

    1

--- bot2.py
+++ bot2.py
@@ -1,13 +1,12 @@
 import os
-from fileinput import filename
 import qrcode
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler,Filters
-from telegram import Chat, InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
+from telegram import  InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
 
 INPUT_TEXT = 0
 
 def start(update, context):
-    update.message.reply_text("Hola ")
+    update.message.reply_text("Hola aaa ")
 
 def qr_command_handler(update, context):
     update.message.reply_text("texto ")
@@ -20,41 +19,28 @@ def generate_qr(text):
     return filename
 
 def send_qr(filename, chat):
-    chat.send_action(action=ChatAction.UPLOAD_PHOTO,timeout=None)
-    chat.send_photo(photo=open(filename,'rb'))
+    chat.send_action(action=ChatAction.UPLOAD_PHOTO)
+    chat.send_photo(photo=open(filename, 'rb'))
     os.unlink(filename)
 
 
 def input_text(update, context):
     text= update.message.text
-    print(text)
-    chat = update.messsage.chat
     filename=generate_qr(text)
-
+    chat = update.messsage.chat    
     send_qr(filename,chat)
     return ConversationHandler.END
-
- #   button1 = InlineKeyboardButton(
-  #      text='sobre el autor',
-   #     url='https://lugofev.com'
-   # )
-    
-  #  update.message.reply_text(
-   ##    reply_markup=InlineKeyboardMarkup([
-     #       [button1]
-      #      ])
-    #)
 
 if __name__ == '__main__':
     
     updater = Updater('5246990550:AAEdax6SS8kbL385Ttift8HvfArdLhX6W8w', use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler( CommandHandler('start', start))
+    dp.add_handler(CommandHandler('start', start))
 
-    dp.add_handler(entry_points=[CommandHandler('qr',qr_command_handler)]),
-    states={INPUT_TEXT: [MessageHaandler(Filters.text,input_text)]},
-    fallbacks=[]
+    dp.add_handler(ConversationHandler(entry_points=[CommandHandler('qr',qr_command_handler)],
+    states={INPUT_TEXT: [MessageHandler(Filters.text, input_text)]},
+    fallbacks=[]))
 
     updater.start_polling()
     updater.idle()
