bddbbd76c0073d2dd5789522260bca2607f29093
edgarorb1 <95740372+edgarorb1@users.noreply.github.com>
2022-02-26 23:17:58 -0600

    v1.2

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyMarkup
+
 def start(update, context):
     update.message.reply_text('hola')
+    button1 = InlineKeyboardButton( text='si')
+    button2 = InlineKeyboardButton( text='no')
+    update.message.reply_text('haz clic en un boton', ReplyMarkup=InlineKeyboardButton([button1],[button2]))
+
 if __name__ == '__main__':
     
     updater = Updater(token='5246990550:AAEdax6SS8kbL385Ttift8HvfArdLhX6W8w', use_context=True)
